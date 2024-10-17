776ca47f8ff94d4ba6dd645060eda12f75ea7091
MaximProhorovsky <masyaprohor@gmail.com>
2024-10-17 19:00:41 +0000

    Add files to github repos

---
+++ main.c
@@ -0,0 +1,184 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdint.h>
+
+void print_prompt(){printf("db > ");}
+
+typedef struct{
+    char *buffer;           //input_string
+    size_t buffer_length; 
+    ssize_t input_length;
+}InputBuffer;
+
+InputBuffer* new_input_buffer(){
+    InputBuffer *input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+    
+    return input_buffer;
+}
+
+void close_input_buffer(InputBuffer *input_buffer){
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+void read_input(InputBuffer *input_buffer){
+    ssize_t bytes_read = getline(&(input_buffer->buffer),
+                                 &(input_buffer->buffer_length), 
+                                 stdin);
+    if(bytes_read <= 0){
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}MetaCommandResult;
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer){
+        if(strcmp(input_buffer->buffer, ".exit") == 0){
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }else{
+            return META_COMMAND_UNRECOGNIZED_COMMAND;
+        }
+}
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_SYNTAX_ERROR
+}PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+
+typedef struct{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+}Row;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+typedef struct {
+    StatementType type;
+    Row row_to_insert;
+}Statement;
+
+void serialize_row(Row *source, void *destination){
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(Row *source, void *destination){
+    memcpy(&(destination->id), source + ID_OFFSET,ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct{
+    uint32_t num_rows;
+    void *pages[TABLE_MAX_PAGES];
+}Table;
+
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement){
+    if(strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer->buffer,
+                                   "insert %d %s %s", 
+                                   &(statement->row_to_insert.id),
+                                   statement->row_to_insert.username,
+                                   statement->row_to_insert.email);
+        if(args_assigned < 3){
+            return PREPARE_SYNTAX_ERROR;
+        }
+        return PREPARE_SUCCESS;
+    }
+    if(strcmp(input_buffer->buffer, "select") == 0){
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+    switch(statement->type)
+    {
+        case STATEMENT_INSERT:
+            printf("DO INSERT(TEST!)\n");
+            break;
+        case STATEMENT_SELECT:
+            printf("DO SELECT(TEST!)\n");
+            break;
+    }
+}
+
+int main(int argc, char *argv[]){
+    InputBuffer *input_buffer = new_input_buffer();
+    Statement statement;
+    while(true)
+    {
+        print_prompt();
+        read_input(input_buffer);
+        if(input_buffer->buffer[0] == '.')
+        {
+            switch(do_meta_command(input_buffer))
+            {
+                case META_COMMAND_SUCCESS:
+                    continue;
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                    continue;
+            }
+        }
+        switch(prepare_statement(input_buffer, &statement))
+        {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                printf("Unrecognized keyword at start of '%s'.\n",input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
+    }
+    return 0; 
+}
+
+
